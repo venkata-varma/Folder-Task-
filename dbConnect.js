5eebdb1636b2b758f4fa14ddbddbe87418021ff2
venkata-varma <venkatavarma92@gmail.com>
2023-07-19 21:20:59 +0530

    Implemented Gitignore

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose')
-const db='mongodb+srv://venkats9:weather9@cluster0.9ftgfg0.mongodb.net/Fwhy3?retryWrites=true&w=majority'
+ 
 async function connectDB(){
     try{
-await mongoose.connect(db,{
+await mongoose.connect(process.env.db,{
     useNewUrlParser:true
 })
 console.log('DB is up')
